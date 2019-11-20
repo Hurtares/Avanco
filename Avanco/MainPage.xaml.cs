ccaef6f40a587e66f389a171de80ca3ee2e43ca4
Bruno Filipe Sousa da Silva <brunosilva_95@hotmail.com>
2019-11-20 09:48:40 +0000

    sad
    asdsa

---
+++ Avanco/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Avanco
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+            List<Peca> pecas = new List<Peca>();
+        public MainPage() {
+            this.InitializeComponent();
+            var ellipse1 = new Ellipse();
+            ellipse1.Fill = new SolidColorBrush(Windows.UI.Colors.SteelBlue);
+            ellipse1.Width = 200;
+            ellipse1.Height = 200;
+            ellipse1.Margin = new Thickness(0, 400, 0, 0);
+            RotateBehavior.CenterX = 100;
+            cubo.Margin = new Thickness(10, 10, 0, 0);
+            //layoutRoot.Children.Remove(cubo);
+            DrawBoard();
+            Rectangle temp1 = new Rectangle();
+            temp1.Fill = new SolidColorBrush(Windows.UI.Colors.Beige);
+            temp1.Width = 50;
+            temp1.Height = 50;
+            layoutRoot.Children.Add(temp1);
+            // When you create a XAML element in code, you have to add
+            // it to the XAML visual tree. This example assumes you have
+            // a panel named 'layoutRoot' in your XAML file, like this:
+            // <Grid x:Name="layoutRoot>
+            layoutRoot.Children.Add(ellipse1);
+            for (int i = 0; i < 14; i++) {
+                var test = new Peca(false,i);
+                layoutRoot.Children.Add(test.Ellipse);
+                pecas.Add(test);
+            }
+            for (int i = 35; i < 49; i++) {
+                var test = new Peca(true, i);
+                layoutRoot.Children.Add(test.Ellipse);
+                pecas.Add(test);
+            }
+        }
+        private void DrawBoard() {
+            for (int i = 0; i < 7; i++) {
+                for (int j = 0; j < 7; j++) {
+                    Rectangle temp = new Rectangle();
+                    temp.Fill = new SolidColorBrush(Windows.UI.Colors.AliceBlue);
+                    temp.Width = 100;
+                    temp.Height = 100;
+                    temp.VerticalAlignment = VerticalAlignment.Top;
+                    temp.HorizontalAlignment = HorizontalAlignment.Left;
+                    temp.Margin = new Thickness(40 + i * 120, 40 + j * 120, 0, 0);
+                    layoutRoot.Children.Add(temp);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            layoutRoot.Children.Remove(pecas[4].Ellipse);
+        }
+    }
+}
---
+++ Avanco/Peca.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Avanco
+{
+    class Peca
+    {
+        public bool White { get; }
+        public int Position { get; set; }
+        public Ellipse Ellipse { get; set; }
+
+        public Peca(bool white,int position) {
+            White = white;
+            Position = position;
+            CreatePeace();
+        }
+
+        private void CreatePeace() {
+            int i = Position % 7;
+            int j = Position/7;
+            Ellipse = new Ellipse {
+                Name = Position.ToString(),
+                Fill = new SolidColorBrush(Windows.UI.Colors.Black),
+                Width = 80,
+                Height = 80,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(50 + i * 120, 50 + j * 120, 0, 0)
+            };
+            if (White) {
+                Ellipse.Fill = new SolidColorBrush(Windows.UI.Colors.BurlyWood);
+            } else {
+                Ellipse.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+        }
+    }
+}
